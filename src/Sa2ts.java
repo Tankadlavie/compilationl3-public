19c101f0b2637110b0eaa0ecd2f4e81c851eb0b1
Tankadlavie <tankadlavie.prod@gmail.com>
2020-04-07 18:50:16 +0200

    Modif de Sa2Ts

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -44,40 +44,22 @@ public class Sa2ts extends SaDepthFirstVisitor <Void>{
 
     @Override
     public Void visit(SaDecVar node) {
-        System.out.println("DECVAR");
-        if (existeVAR(node.tsItem.identif)){
-            System.out.println("FAILED");
-            System.exit(1);
-        }
-        if (Table_locale==null)
-        {
-             node.tsItem = Table_De_Symbole_Globale.addParam(node.getNom());
-		}
-		else
-		{
-		    node.tsItem = Table_locale.addParam(node.getNom());
-		}
+        if(Table_locale != null)
+            node.tsItem = Table_locale.addVar(node.getNom(),1);
+        else
+            node.tsItem = Table_De_Symbole_Globale.addVar(node.getNom(),1);
         return null;
     }
 
     @Override
     public Void visit(SaDecTab node) {
-        System.out.println("DECTAB");
-        if (Table_locale != null){
-            System.out.println("FAILED");
-            System.exit(1);
-        }
-
-        if (existeVARglob(node.getNom())){
-            System.out.println("FAILED");
-            System.exit(1);
-        }
-
-        node.tsItem = Table_De_Symbole_Globale.addVar(node.getNom(),node.getTaille());
-
+        if(Table_locale != null)
+            node.tsItem = Table_locale.addVar(node.getNom(), node.getTaille());
+        else
+            node.tsItem = Table_De_Symbole_Globale.addVar(node.getNom(), node.getTaille());
         return null;
     }
-
+	
     @Override
     public Void visit(SaDecFonc node) {
         System.out.println("DECFONC");
@@ -85,11 +67,6 @@ public class Sa2ts extends SaDepthFirstVisitor <Void>{
             System.out.println("FAILED");
             System.exit(1);
         }
-        System.out.println("TEST 1 DECFONC REUSSI");
-
-        System.out.println("oui");
-        System.out.println("oui");
-        System.out.println("oui");
 
         if (existeFONC(node.getNom())){
             System.out.println("FAILED");
@@ -114,43 +91,34 @@ public class Sa2ts extends SaDepthFirstVisitor <Void>{
             node.getCorps().accept(this);
         }
         node.tsItem = Table_De_Symbole_Globale.addFct(node.getNom(),temp,this.Table_locale,node);
-        System.out.println("tsItem crée");
 
-        System.out.println("Mise a null de la table locale");
         this.Table_locale=null;
-        System.out.println("Table local = null" );
 
         return null;
     }
 
     @Override
     public Void visit(SaVarSimple node) {
-        if (!existeVAR(node.tsItem.identif) && !existeVARglob(node.tsItem.identif))
-            System.exit(1);
-        /*
-        if (existeVAR(node.tsItem.identif)) // ??
-        {
-            if (Table_locale.getVar(node.tsItem.identif).getTaille()!=1)
-            {
-                System.exit(1);
-            }
+        if(Table_locale.variables.containsKey(node.getNom())) {
+            if (Table_locale.getVar(node.getNom()).getTaille() != 1)
+                throw new UnsupportedOperationException("La variable n'existe pas");
+            node.tsItem = Table_locale.getVar(node.getNom());
         }
-        if (existeVARglob(node.tsItem.identif)) // ??
-        {
-            if (Table_De_Symbole_Globale.getVar(node.tsItem.identif).getTaille()!=1)
-                System.exit(1);
+        if(Table_De_Symbole_Globale.variables.containsKey(node.getNom())){
+            if(Table_De_Symbole_Globale.getVar(node.getNom()).getTaille() != 1)
+                throw new UnsupportedOperationException("La variable n'existe pas");
+            node.tsItem = Table_De_Symbole_Globale.getVar(node.getNom());
         }
-        */
         return null;
-	}
+    }
     
     @Override 
     public Void visit(SaVarIndicee node) {
+        /*
         if (!existeVAR(node.tsItem.identif) && !existeVARglob(node.tsItem.identif)){
             System.out.println("FAILED");
             System.exit(1);
         }
-        /*
         if (existeVAR(node.tsItem.identif)) // ??
         {
             if (Table_locale.getVar(node.tsItem.identif).getTaille()==1)
@@ -164,8 +132,18 @@ public class Sa2ts extends SaDepthFirstVisitor <Void>{
                 System.exit(1);
         }
         */
+		if (node.getNom()!=null)
+		{
+			node.tsItem = Table_locale.addParam(node.getNom());
+		}
+		else 
+		{
+			node.tsItem = Table_locale.addParam("YA QUEDAL");
+			
+		}
+        node.tsItem = Table_De_Symbole_Globale.addVar(node.getNom(),node.tsItem.taille);
 		return null;
-	}
+	}	
 
 	@Override
     public Void visit(SaAppel node ){
